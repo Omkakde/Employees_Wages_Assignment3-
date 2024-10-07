7933fdeaf11310a56005f7a0553ec7207eb16cb5
Omkakde <kakadeom123@gmail.com>
2024-10-07 11:25:04 +0530

    employee wages structure code

--- employeeWages.js
+++ employeeWages.js
@@ -1,1 +1,19 @@
-console.log("Welecome to employee wage computation program");
+class Staff {
+    constructor(empName, empId) {
+        this.empName = empName;  
+        this.empId = empId;      
+        this.attendanceStatus = generateRandom();  
+    }
+
+    checkAttendance() {
+        return this.attendanceStatus ? "Present" : "Absent";
+    }
+
+    showDetails() {
+        console.table([{ Name: this.empName, ID: this.empId, Attendance: this.checkAttendance() }]);
+    }
+}
+
+
+
+
